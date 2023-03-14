1342f83d6e77e42660dc2b269291bcda6d3f88b8
Rafael Sanzio <rafaelsanzio27@gmail.com>
2023-03-14 08:17:08 -0300

    feat: pkg for kafka consume
    Adding a new package for kafka consumer

---
+++ pkg/kafka/kafka.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func Consume(topics []string, servers string, msgCahn chan *ckafka.Message) {
+	kafkaConsumer, err := ckafka.NewConsumer(&ckafka.ConfigMap{
+		"bootstrap.servers": servers,
+		"group.id":          "goapp",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = kafkaConsumer.SubscribeTopics(topics, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		msg, err := kafkaConsumer.ReadMessage(-1)
+		if err == nil {
+			msgCahn <- msg
+		}
+	}
+}
